760f0da2e5293e9601b48699142ba7daa8aa1822
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-05-16 20:14:30 +0530

    Homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,17 @@
 import "./Homepage.css";
 import {Categories} from "./components/Categories";
+import {Link} from "react-router-dom";
 
 function Homepage(){
 
     return(
         <div>
+            <div className="main">
+                <p className="main-text">Music is the key thing in everyone's life, Which can change any kind of mood.</p>
+                <Link to="/videos"><button className="button primary-blue">Explore music</button></Link>
+                <p className="main-text">Explore,watch,listen and enjoy...😉</p>
+            </div>
             <Categories />
-            
         </div>
     )
 }
