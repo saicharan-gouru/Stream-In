8c4e17b7e2a6b543f550098b3f7d01f98f93fde1
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-05-19 09:31:34 +0530

    added videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -3,19 +3,70 @@
  * You can add videos of your wish with different attributes
  * */
 
-export const videos = [
-  {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
-  },
-  {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  },
+export const videos = [{
+        _id: "Wo5dMEP_BbI",
+        title: "Nuvvani Idhi Needani Full Video Song || Maharshi Songs || MaheshBabu || VamshiPaidipally",
+        description: "Watch & Enjoy #NuvvaniIdhiNeedani Full Video Song From #Maharshi Movie.",
+        creator: "Aditya Music",
+        thumbnail: "https://i.ytimg.com/vi/4dbiE-da82E/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDNQDPCRPz1idPDNKhdmCN0ZOBPPg",
+        avatar: "https://yt3.ggpht.com/ytc/AKedOLReKISHylSA5PAVOXXnQycmveRc6NdoQT661kJswg=s48-c-k-c0x00ffffff-no-rj",
+        views: "2C",
+        duration: "4:00",
+        categoryName: "Tollywood"
+    },
+    {
+        _id: "F_Riqjdh2oM",
+        title: "Choopultho - Radha | Sharwanand | Lavanya | Aksha",
+        creator: "Times Music South",
+        description: "#Radha ft. Sharwanand & Lavnya Tripathi. Music composed by Radan and directed by Chandra Mohan. Movie is Produced by Bhogavalli Baapineedu.",
+        thumbnail: "https://i.ytimg.com/vi/W_cNJEaW6jg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCnHaRc5KvmdDC_jWmHsKxwvLFRzQ",
+        avatar: "https://yt3.ggpht.com/pBSC4REluv1kfFoFCFyCCL6yFo2RX3om4irYVowOylRBfbboyMfEpqgE9Km8nZosxmOt8eBP1Q=s48-c-k-c0x00ffffff-no-rj",
+        views: "18L",
+        duration: "3:50",
+        categoryName: "Tollywood"
+    },
+    {
+        _id: "Q_Riqjdh2oM",
+        title: "#PataasPilla Full Video Song | DJTillu | Siddhu, Neha Shetty ",
+        creator: "Aditya Music",
+        description: "Watch & Enjoy Pataas Pilla Full Video Song From The Movie #DJTillu. ",
+        thumbnail: "https://i.ytimg.com/vi/VdGY5cs7onc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDIOC4aoW_PmOZh4HGA4Gsj3qQUzg",
+        avatar: "https://yt3.ggpht.com/ytc/AKedOLReKISHylSA5PAVOXXnQycmveRc6NdoQT661kJswg=s48-c-k-c0x00ffffff-no-rj",
+        views: "1.1C",
+        duration: "3:31",
+        categoryName: "Tollywood"
+    },
+    {
+        _id: "A_Stqjdh2oM",
+        title: "Channa Mereya (unplugged with Lyrics) : Arijit Singh |",
+        creator: "Infinity Saga",
+        description: "Watch & Enjoy Pataas Pilla Full Video Song From The Movie #DJTillu. ",
+        thumbnail: "https://i.ytimg.com/vi/SQSR8-PftwA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB_fMlnkOAAzDIwaKNpvvlEMJc8vg",
+        avatar: "https://yt3.ggpht.com/WkDJxNndzmmBIQJGV_elWu40bhk8ZOG21WRXxe3Cqam3SErDHZ2j0JwEPjFGrDng2O9U0CnXVw=s176-c-k-c0x00ffffff-no-rj",
+        views: "27L",
+        duration: "2:44",
+        categoryName: "Bollywood"
+    },
+    {
+        _id: "V_Stdjah2ji",
+        title: "Ranjha  Official Video | Shershaah | Sidharth Kiara ",
+        creator: "Sony Music India",
+        description: "When you've finally gotten to be with the love of your life, how do you say goodbye so soon?. ",
+        thumbnail: "https://i.ytimg.com/vi/SQSR8-PftwA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB_fMlnkOAAzDIwaKNpvvlEMJc8vg",
+        avatar: "https://yt3.ggpht.com/vJEsALWnQIwPZ2oaVbFWPu1n4L0CDkHpAdGJ-KBPFsxM9hsMsvExFK1mnCGUrMzfC67XPVThmQ=s176-c-k-c0x00ffffff-no-rj",
+        views: "25C",
+        duration: "3:14",
+        categoryName: "Bollywood"
+    },
+    {
+        _id: "W_Stdjam5ji",
+        title: "Pal Pal Dil Ke Paas - Title | Arijit Singh | Karan Deol, Sahher ",
+        creator: "Zee Music Company",
+        description: "When you've finally gotten to be with the love of your life, how do you say goodbye so soon?. ",
+        thumbnail: "https://i.ytimg.com/vi/lgTHGZF3BQw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDP7mScJBXBlgFhXWQw9QMvQKxSHw",
+        avatar: "https://yt3.ggpht.com/EEGERwlaKJd27zSEPQF3d__-tPyppIgFfKvNfBkWa7ssMKBWqQUbuCTLe-kAnTB1r6kJQVxyxwY=s176-c-k-c0x00ffffff-no-rj",
+        views: "19C",
+        duration: "3:03",
+        categoryName: "Bollywood"
+    },
 ];
