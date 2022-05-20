b333476f014a7c9ea6bc8cdc2502e7182af6e89c
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-05-20 18:28:17 +0530

    added video cards

--- src/pages/Videoslisting/Videolisting.js
+++ src/pages/Videoslisting/Videolisting.js
@@ -8,7 +8,20 @@ function Videolisting(){
     return(
         <div>
             <h1>Videos</h1>
-            {videos.map(item => <li>{item.title}</li>)}
+        <div className="videos-container">
+            {videos.map(item => 
+            <div className="video-card">
+                <img src={item.thumbnail} className="thumbnail" alt={item.categoryName} />
+                <h3>{item.title} </h3>
+                <small>{item.views} views</small>
+                <div className="creator-details">
+                    <img src={item.avatar} className="avatar" />
+                    <h3>{item.creator}</h3>
+                </div>
+               
+            </div>
+            )}
+        </div>
         </div>
     )
 }
