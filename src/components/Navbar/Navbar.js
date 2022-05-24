a82502b6400b947090d7e61f871c93cea9ada65f
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-05-24 19:05:22 +0530

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ function Navbar(){
     return(
         <div className="nav-bar">
             <Link to="/" className="title"><h1>🎞️🍿 Stream In  👀📽</h1></Link>
-            {user ? <button className="button primary-green nav-link hover-effect" onClick={logoutHandler} >Logout</button> : <Link to="/login" className="login-btn">Login/Signup</Link>}
+            {user ? <button className="button primary-green hover-effect logout-btn" onClick={logoutHandler} >Logout</button> : <Link to="/login" className="login-btn">Login/Signup</Link>}
         </div>
     )
 }
