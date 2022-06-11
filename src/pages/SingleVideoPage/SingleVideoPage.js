6ec58ea8e80314391824c5bccbf395d3cee782dc
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-06-11 11:18:37 +0530

    added watchlater api calls

--- src/pages/SingleVideoPage/SingleVideoPage.js
+++ src/pages/SingleVideoPage/SingleVideoPage.js
@@ -6,12 +6,17 @@ import {useDocumentTitle} from "../../customhooks";
 import axios from "axios";
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
 import { useState } from "react";
+
 
 function SingleVideoPage(){
     const { videos } = useData();
     const { _id } = useParams();
     const [videosInLiked, setVideosInLiked] = useState([]);
+    const [videosInWatchlater,setVideosInWatchlater] = useState([]);
+    const [watchlater,setWatchlater] = useState(false);
     const [liked,setLiked] = useState(false);
     const video = videos.find((item) => item._id===_id);
     const getSingleVideo = video;
@@ -30,6 +35,25 @@ function SingleVideoPage(){
         } catch (error) {
             console.log(error)
         }
+    }
+
+    const getAllVideoInWatchlater = async () => {
+        try {
+            const data = await axios.get("/api/user/watchlater", {
+                headers: { authorization: encodedToken }
+            })
+            setVideosInWatchlater(data.data.watchlater)
+            console.log(data.data.watchlater)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const isInWatchlater = (video) => {
+        if(videosInWatchlater.find(item => item._id === video._id))
+        setWatchlater(true)
+        else
+        setWatchlater(false)
     }
 
     const isLiked = (video) => {
@@ -57,6 +81,15 @@ function SingleVideoPage(){
         }
     }
 
+    const addVideoToWatchlater = async () => {
+        try {
+            await axios.post("/api/user/watchlater", { video },
+                { headers: { authorization: encodedToken } })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     const deleteFromLikedHandler = async (_id) => {
         try{
             const data = await axios.delete(`/api/user/likes/${_id}`,{
@@ -69,10 +102,24 @@ function SingleVideoPage(){
         }
     }
 
+    const deleteFromWatchlaterHandler = async (_id) => {
+        try{
+            const data = await axios.delete(`/api/user/watchlater/${_id}`,{
+                headers: {authorization: encodedToken}
+            })
+            setVideosInWatchlater(data.data.watchlater);
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
     useEffect(() => {
     addVideoToHistory();
     isLiked(video); 
-    getAllVideoInLiked();       
+    isInWatchlater(video);
+    getAllVideoInLiked(); 
+    getAllVideoInWatchlater();      
     })
 
     
@@ -91,7 +138,10 @@ function SingleVideoPage(){
                     </iframe>    
                     <div className="video-details">
                         <h2>{getSingleVideo.title}</h2>
-                        {liked ? <ThumbUpIcon className="like-icon" onClick={()=>deleteFromLikedHandler(video._id)}></ThumbUpIcon> : <ThumbUpOutlinedIcon className="like-icon" onClick={addvideoToLiked}></ThumbUpOutlinedIcon> }
+                        <div>
+                            {liked ? <ThumbUpIcon className="like-icon" onClick={()=>deleteFromLikedHandler(video._id)}></ThumbUpIcon> : <ThumbUpOutlinedIcon className="like-icon" onClick={addvideoToLiked}></ThumbUpOutlinedIcon> }
+                            {watchlater ? <WatchLaterIcon className="watchlater-icon" onClick={()=>deleteFromWatchlaterHandler(video._id)}></WatchLaterIcon> : <WatchLaterOutlinedIcon className="watchlater-icon" onClick={addVideoToWatchlater} ></WatchLaterOutlinedIcon> }
+                        </div>
                         <small>{getSingleVideo.views}+ views</small>
                         <p>{getSingleVideo.description}</p>
                         <div className="creator-details">
--- src/pages/Watchlaterpage/Watchlater.js
+++ src/pages/Watchlaterpage/Watchlater.js
@@ -1,9 +1,33 @@
 import "./Watchlater.css";
 import {useDocumentTitle} from "../../customhooks";
+import axios from "axios";
+import {useEffect,useState} from "react";
 
 function Watchlater(){
-    
+
     useDocumentTitle("Stream In | Watchlater");
+    const [videosInWatchlater, setVideosInWatchlater] = useState([])
+    const encodedToken = localStorage.getItem("token")
+    
+
+    const getAllVideoInWatchlater = async () => {
+        try {
+            const data = await axios.get("/api/user/watchlater", {
+                headers: { authorization: encodedToken }
+            })
+            setVideosInWatchlater(data.data.watchlater)
+            console.log(data.data.watchlater)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
+    useEffect(() => {
+        getAllVideoInWatchlater()
+    })
+
 
     return(
         <div>
