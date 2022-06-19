4062e34c219f978535687e6f246419188e5cc1bb
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-06-19 17:39:46 +0530

    Added playlist functionalities

--- src/pages/Playlistpage/Playlistpage.js
+++ src/pages/Playlistpage/Playlistpage.js
@@ -1,10 +1,58 @@
 import "./Playlistpage.css";
+import {useDocumentTitle} from "../../customhooks";
+import {useState,useEffect} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+import DeleteIcon from '@mui/icons-material/Delete';
+import {Default} from "../../components";
 
 function Playlistpage(){
+    useDocumentTitle("Stream In | Playlists");
+    const [playlists, setPlaylists] = useState([])
+    const encodedToken = localStorage.getItem("token")
+
+    const getAllPlaylists = async () => {
+        try {
+            const data = await axios.get("/api/user/playlists", {
+                headers: { authorization: encodedToken }
+            })
+            console.log(data.data.playlists);
+            setPlaylists(data.data.playlists)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const deletePlaylist = async (_id) => {
+        try{
+            const data = await axios.delete(`/api/user/playlists/${_id}`, {
+                headers: { authorization: encodedToken }
+            })
+            setPlaylists(data.data.playlists)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+
+    useEffect(() => {
+        getAllPlaylists()
+    })
 
     return(
         <div>
             <h1>Playlists</h1>
+        <div className="playlists-container">
+            {playlists.length === 0 && <Default />}
+            {playlists.map(item => 
+                <div>
+                <Link className="playlist" to={`/playlists/${item._id}`}>
+                    {item.title}
+                </Link>
+                <DeleteIcon onClick={()=>deletePlaylist(item._id)}></DeleteIcon>
+                </div>
+            )}
+        </div>
         </div>
     )
 }
--- src/pages/SinglePlaylistpage/SinglePlaylistpage.js
+++ src/pages/SinglePlaylistpage/SinglePlaylistpage.js
@@ -1,10 +1,71 @@
+import { useState, useEffect } from "react";
 import "./SinglePlaylistpage.css";
+import axios from "axios";
+import {useParams,Link} from "react-router-dom";
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function SinglePlaylistpage(){
+    const { _id } = useParams();
+
+    const [videosInPlaylist,setVideosInPlaylist] = useState([]);
+    const encodedToken = localStorage.getItem('token');
+
+
+    const getAllVideoInPlaylist = async () => {
+        try {
+            const data = await axios.get(`/api/user/playlists/:${_id}`, {
+                headers: { authorization: encodedToken }
+            })
+            setVideosInPlaylist(data.data.playlist.videos)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const deleteFromPlaylist = async (_vid) => {
+        try{
+            const data = await axios.delete(`/api/user/playlists/${_id}/${_vid}`, {
+                headers: { authorization: encodedToken }
+            })
+            setVideosInPlaylist(data.data.playlist.videos)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
+
+
+    useEffect(()=>{
+        getAllVideoInPlaylist();
+    })
+
+
 
     return(
         <div>
             <h1>Single playlist page</h1>
+            <div className="videos-container">
+            { videosInPlaylist.map(item => 
+                <div className="history-video-card" key={item._id}>
+                    <div className="history-video-card-container-1">
+                        <img src={item.thumbnail} className="thumbnail" alt={item.categoryName} />
+                        <span className="duration">{item.duration}</span>
+                    </div>
+                    <div className="history-video-card-container-1">
+                        <h3>{item.title} </h3>
+                        
+                        <div className="creator-details">
+                            <img src={item.avatar} className="avatar" alt="avatar" />
+                            <h3>{item.creator}</h3>  
+                        </div>
+                        <Link to={`/video/${item._id}`}>Go to video</Link>
+                    </div>
+                    <DeleteIcon className="delete-icon" onClick={()=>deleteFromPlaylist(item._id)}></DeleteIcon>
+                </div>
+                 )}
+            </div>
         </div>
     )
 }
